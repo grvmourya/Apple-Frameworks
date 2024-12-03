effb7b0065ad1c6da9c7cba7ca1c1d831d590194
grvmourya <gauravmourya1999@gmail.com>
2024-12-04 00:35:49 +0530

    Initial Commit

---
+++ Apple-Frameworks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple-Frameworks
+//
+//  Created by Gaurav Mourya on 04/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
