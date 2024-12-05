d0311b7c51e9a20524380726118b3091f0dbdb19
grvmourya <gauravmourya1999@gmail.com>
2024-12-05 19:24:47 +0530

    Frameworks App Completed

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -10,13 +10,11 @@ import SwiftUI
 struct FrameworkGridView: View {
     @StateObject var viewModel = FrameworkGridViewModel()
     
-    let columns: [GridItem] = [GridItem(.flexible()),
-                            GridItem(.flexible()),
-                            GridItem(.flexible())]
+    
     var body: some View {
         NavigationView{
             ScrollView{
-                LazyVGrid(columns: columns){
+                LazyVGrid(columns: viewModel.columns){
                     ForEach(MockData.frameworks, id: \.id){ framework in
                         FrameworkTitleView(framework: framework)
                             .onTapGesture {
@@ -25,7 +23,7 @@ struct FrameworkGridView: View {
                     }
             }
             }
-            .navigationTitle("FrameWork Apps")
+            .navigationTitle("FrameWorks")
             .sheet(isPresented: $viewModel.isShowingDetailView)
             {
                 FrameworkDetailView(framework: viewModel.selectedFramework ?? MockData.sampleFramework,
@@ -41,22 +39,4 @@ struct FrameworkGridView: View {
 #Preview {
     FrameworkGridView()
         .preferredColorScheme(.dark)
-}
-
-struct FrameworkTitleView: View {
-    let framework: Framework
-    
-    var body: some View {
-        VStack{
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90,height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-        }
-        .padding()
-    }
 }
---
+++ Apple-Frameworks/XDismissButton.swift
@@ -0,0 +1,32 @@
+//
+//  XDismissButton.swift
+//  Apple-Frameworks
+//
+//  Created by Gaurav Mourya on 05/12/24.
+//
+
+import SwiftUI
+
+struct XDismissButton: View {
+    
+    @Binding var isShowingDetailView: Bool
+    var body: some View {
+        HStack{
+            Spacer()
+            Button{
+                isShowingDetailView = false
+            }label: {
+                Image(systemName: "xmark")
+                    .foregroundColor(Color(.label))
+                    .imageScale(.large)
+                    .frame(width: 44,height: 44)
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    XDismissButton(isShowingDetailView: .constant(false))
+}
